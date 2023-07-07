f5deea80673a367dca884565abebb9722dca6c4d
cesar <cesarcasas0737@gmail.com>
2023-07-07 16:49:57 -0700

    created functional contact page and linked

---
+++ src/contact.js
@@ -0,0 +1,19 @@
+import createParagraph from "./createParagraph";
+
+function createContact() {
+    const contact = document.createElement("div");
+    contact.classList.add("contact");
+
+    contact.appendChild(createParagraph("✆1(800)123-4567"));
+    contact.appendChild(createParagraph("🍽 1768 E Hastings St,Vancouver,Los Angeles, USA"));
+
+    return contact;
+}
+
+function loadContact() {
+    const main = document.getElementById("main");
+    main.textContent = "";
+    main.appendChild(createContact());
+  }
+
+  export default loadContact;
--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import createParagraph from "./createParagraph";
+
 function createHome() {
   const home = document.createElement("div");
   home.classList.add("home");
@@ -14,11 +16,6 @@ function createHome() {
   return home;
 }
 
-function createParagraph(text) {
-  const paragraph = document.createElement("p");
-  paragraph.textContent = text;
-  return paragraph;
-}
 
 function loadHome() {
   const main = document.getElementById("main");
