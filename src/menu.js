9ebd6cb694bfc71fef728c5fb2dcc087aae1d8ce
cesar <cesarcasas0737@gmail.com>
2023-07-07 18:34:05 -0700

    Added createMenueItem that adds on to menu

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,48 @@ function createMenu(){
     const menu = document.createElement("div");
     menu.classList.add("menu");
 
+    menu.appendChild(
+        createMenuItem(
+            "bulgogi",
+            "불고기",
+            "Thinly sliced ribeye beef marinated in specially formulated house sauce"
+        )
+    );
+
     return menu;
+}
+
+function createMenuItem(engName,korName,description) {
+    const menuItem = document.createElement("div");
+    menuItem.classList.add("menu-item");
+
+
+    const foodImage = document.createElement("img");
+    foodImage.src = `media/images/${engName.toLowerCase()}.png`;
+    foodImage.alt = `${engName}`;
+    menuItem.appendChild(foodImage);
+
+
+    const foodTextBox = document.createElement("div");
+    foodTextBox.classList.add("food-text-box");
+    
+
+    const engFoodName = document.createElement("p");
+    engFoodName.textContent = engName;
+
+    const korFoodName = document.createElement("h2");
+    korFoodName.textContent = korName;
+
+    const foodDescription = document.createElement("p");
+    foodDescription.textContent = description;
+
+    foodTextBox.appendChild(engFoodName);
+    foodTextBox.appendChild(korFoodName);
+    foodTextBox.appendChild(foodDescription);
+
+    menuItem.appendChild(foodTextBox);
+
+    return menuItem;
 }
 
 function loadMenu() {
